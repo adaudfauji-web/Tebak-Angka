6320eedb80915f0e27a879c3d2b10448f5755331
adaudfauji-web <a.daudfauji@gmail.com>
2025-10-29 23:05:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let angkaRahasia = Math.floor(Math.random() * 10) + 1;
+console.log("Angka rahasia:", angkaRahasia); // buat ngecek di console
+
+const input = document.getElementById("tebakan");
+const pesan = document.getElementById("pesan");
+const cekBtn = document.getElementById("cekBtn");
+const ulangBtn = document.getElementById("ulangBtn");
+
+cekBtn.addEventListener("click", function() {
+  const tebakan = parseInt(input.value);
+
+  if (isNaN(tebakan) || tebakan < 1 || tebakan > 10) {
+    pesan.textContent = "Masukkan angka 1 - 10 ya Sayang 😘";
+    pesan.style.color = "orange";
+    return;
+  }
+
+  if (tebakan === angkaRahasia) {
+    pesan.textContent = "🎉 Selamat Sayang! Kamu benar 💖";
+    pesan.style.color = "green";
+  } else if (tebakan < angkaRahasia) {
+    pesan.textContent = "Terlalu kecil 😅";
+    pesan.style.color = "red";
+  } else {
+    pesan.textContent = "Terlalu besar 😅";
+    pesan.style.color = "red";
+  }
+});
+
+ulangBtn.addEventListener("click", function() {
+  angkaRahasia = Math.floor(Math.random() * 10) + 1;
+  pesan.textContent = "";
+  input.value = "";
+  console.log("Angka baru:", angkaRahasia);
+});
